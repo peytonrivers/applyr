f8eb938fd2d70ec69e4a7de08d9adde53bd43490
Peyton Rivers <peytonrivers@Peytons-Air-2.lan>
2026-03-07 16:14:43 -0500

    updated CORSMiddleware to allow more origiins

--- main.py
+++ main.py
@@ -24,9 +24,11 @@ app.add_middleware(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://applyr-12k0.onrender.com",
         "https://apply-r.com",
+        "https://www.apply-r.com",
+        "https://applyr-12k0.onrender.com",
         "http://localhost:5500",
+        "http://127.0.0.1:5500",
     ],
     allow_credentials=True,
     allow_methods=["*"],
