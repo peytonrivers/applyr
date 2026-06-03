1386c81872882d72c4542a3c486ef62c37dc5b85
Peyton Rivers <peytonrivers@Peytons-Air-2.lan>
2026-06-02 22:52:57 -0400

    updated storage to be more protected when uploading to supabase storage

--- database/storage.py
+++ database/storage.py
@@ -5,6 +5,6 @@ from supabase import create_client, Client
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
