ed7e7ae0fc89c1c0c906fccd86b332d1021c4426
Peyton Rivers <peytonrivers716@gmail.com>
2026-08-17 15:46:15 -0400

    updated requirements again

--- try.py
+++ try.py
@@ -1,5 +1,20 @@
 from playwright.sync_api import sync_playwright
 import json
+import psutil
+import tracemalloc
+
+memory = psutil.virtual_memory()
+cpu_usage = psutil.cpu_percent(interval=1)
+
+tracemalloc.start()
+snapshot = tracemalloc.take_snapshot()
+statisticts = snapshot.statistics('lineno')
+
+for stat in statisticts[:10]:
+    print(f"stat: {stat}")
+
+print(f"ram memory: {memory.percent}%")
+print(f"cpu usage: {cpu_usage}%")
 
 def math_process(a: int, b: int):
     c = a + b
